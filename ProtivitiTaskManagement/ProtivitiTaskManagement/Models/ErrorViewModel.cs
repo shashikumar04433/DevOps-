cdff7fefb3f73583da39171b8386a7396fedb1ca
Shashi Reddy (IND) <shashi.reddy@protivitiglobal.in>
2023-08-03 19:51:03 +0530

    add the ecr-docker command files

--- ProtivitiTaskManagement/ProtivitiTaskManagement/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProtivitiTaskManagement.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
