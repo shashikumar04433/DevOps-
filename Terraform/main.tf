439d7d95276d3af86dd74adb3477cefcb05e716a
shashikumar reddy <55330284+shashikumar04433@users.noreply.github.com>
2023-11-24 14:45:44 +0530

    Add files via upload

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,11 +1,24 @@
 provider "aws" {
-    region="ap-south-1"
-  
+  alias  = "ap-south-1"
+  region = "ap-south-1" # Set your desired AWS region
 }
-module "ec2_instance" {
-    source = "./module/ec2_instance"
-    ami_value="ami-02a2af70a66af6dfb"
-    instance_type_value="t2.micro"
 
-    
+provider "aws" {
+  alias  = "ap-northeast-3"
+  region = "ap-northeast-3" # Set your desired AWS region
 }
+
+resource "aws_instance" "Example" {
+  ami           = "ami-02a2af70a66af6dfb" # Specify an appropriate AMI ID
+  instance_type = "t2.micro"
+  provider      = aws.ap-south-1
+  key_name      = "terraform"
+}
+resource "aws_instance" "Example2" {
+  ami           = "ami-0690c54203f5f67da" # Specify an appropriate AMI ID
+  instance_type = "t2.micro"
+  provider      = aws.ap-northeast-3
+  key_name      = "terra"
+}
+
+  
