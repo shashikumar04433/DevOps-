d01d3c9e94ad2d317369e5536cd5583daf6babc8
shashikumar reddy <55330284+shashikumar04433@users.noreply.github.com>
2024-11-16 21:03:18 +0530

    Create varibles_vm.tf

---
+++ Terraform/varibles_vm.tf
@@ -0,0 +1,9 @@
+variable "admin_username" {
+  description = "The admin username for the VM"
+  default     = "adminuser"
+}
+
+variable "admin_password" {
+  description = "The admin password for the VM"
+  default     = "XXXXXXXX"
+}
